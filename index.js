a041971d29369a87c8bfd7db4c6874098a4c9bcb
AC3ROZ <iori_16n1300006@nnn.ed.jp>
2016-07-04 01:29:49 +0900

    post

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 'use strict';
-
+var client = require('cheerio-httpcli');
 var express = require('express');
 var app = express();
+const mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user : 'root',
+    database: 'proto_bookmark'
+});
+
 const bodyParser = require('body-parser');
 
 app.use(bodyParser());
@@ -16,6 +23,20 @@ app.get('/add', (req, res) => {
 });
 app.post('/add', (req, res) => {
     console.log(req.body);
+    var url = req.body.url;
+    client.fetch(url, {}, (err, $, res) =>{
+        if(err){
+            console.log(err);
+            return 0;
+        }
+        let title = $('title').text();
+        connection.query('INSERT INTO `bookmark` (`url`, `page_title`,`_comment`) VALUES (?,?,?)'
+            ,[url, title, req.body.comment], (err, result, fields) =>{
+                if(err){
+                    console.log(err);
+                }
+            });
+    });   
 });
 
 app.listen(3000);
