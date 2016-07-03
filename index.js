516e3648219aade8cb7b65d2aaa75f55ae3da910
AC3ROZ <iori_16n1300006@nnn.ed.jp>
2016-07-03 23:08:32 +0900

    introduce project into jade.

--- index.js
+++ index.js
@@ -3,8 +3,11 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'jade')
+app.set('views', __dirname + '/views')
+
 app.get('/', (req, res)=>{
-    res.send('HelloWorld');
+    res.render('index', {title: 'Himanoa'});
 });
 
 app.listen(3000);
