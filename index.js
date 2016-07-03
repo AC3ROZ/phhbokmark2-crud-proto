2b4e891215b94eac2e0b7713681645c94441e335
AC3ROZ <iori_16n1300006@nnn.ed.jp>
2016-07-04 01:04:16 +0900

    bodyparser

--- index.js
+++ index.js
@@ -2,12 +2,20 @@
 
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser());
 app.set('view engine', 'jade')
 app.set('views', __dirname + '/views')
 
 app.get('/', (req, res)=>{
     res.render('index', {title: 'Himanoa'});
+});
+app.get('/add', (req, res) => {
+    res.render('add');
+});
+app.post('/add', (req, res) => {
+    console.log(req.body);
 });
 
 app.listen(3000);
